e8d37d9b34eb04ec890aeb0dff9d2a90d278e2cd
Junrel <junrel.mdevocion@gmail.com>
2021-07-24 22:32:44 +0800

    added icon in mobile view

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -32,8 +32,8 @@ const Navbar: NextPage<NavbarProps> = ({collapsed, setCollapsed, token, username
 				<div className="navbar-collapse collapse">
 					<ul className="navbar-nav navbar-align">
 						<li className="nav-item dropdown">
-							<a className="nav-icon dropdown-toggle d-inline-block d-sm-none"  href="#" data-bs-toggle="dropdown">
-								<i className="align-middle" data-feather="settings"></i>
+							<a className="nav-icon dropdown-toggle d-inline-block d-sm-none"  href="#" data-bs-toggle="dropdown" onClick={dropdownToggler}>
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-settings align-middle me-2"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>
 							</a>
 
 							<a className="nav-link d-none d-sm-flex align-items-center py-0" href="#" data-bs-toggle="dropdown" onClick={dropdownToggler}>
